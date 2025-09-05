25d11c72ba4fd1767c6de7b2a4fa9334ab74dc41
kvaibhav055 <167538291+kvaibhav055@users.noreply.github.com>
2025-09-05 23:22:09 +0530

    Update settings.py

--- me_api_backend/settings.py
+++ me_api_backend/settings.py
@@ -40,15 +40,12 @@ ROOT_URLCONF = "me_api_backend.urls"
 
 
 DATABASES = {
-"default": {
-"ENGINE": "django.db.backends.mysql",
-"NAME": env("DB_NAME"),
-"USER": env("DB_USER"),
-"PASSWORD": env("DB_PASSWORD"),
-"HOST": env("DB_HOST"),
-"PORT": env("DB_PORT"),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
-}
+
 
 
 CORS_ALLOWED_ORIGINS = env.list("CORS_ORIGIN_WHITELIST", default=[])
